8a2a70eac2c585a69eda6752d3af2debc918359e
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2026-06-24 21:42:06 +0300

    Configure Hilt and clean up MainActivity boilerplate
    - Annotate `MusicPlayerApplication` with `@HiltAndroidApp`.
    - Annotate `MainActivity` with `@AndroidEntryPoint`.
    - Remove default `Greeting` composable and preview code from `MainActivity`.

--- app/src/main/java/com/androidinternals/musicplayer/MainActivity.kt
+++ app/src/main/java/com/androidinternals/musicplayer/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.androidinternals.musicplayer.ui.theme.MusicPlayerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +22,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             MusicPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicPlayerTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/androidinternals/musicplayer/MusicPlayerApplication.kt
+++ app/src/main/java/com/androidinternals/musicplayer/MusicPlayerApplication.kt
@@ -1,6 +1,7 @@
 package com.androidinternals.musicplayer
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-class MusicPlayerApplication: Application() {
-}
+@HiltAndroidApp
+class MusicPlayerApplication: Application()
