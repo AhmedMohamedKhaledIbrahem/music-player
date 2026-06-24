314141f81780430f84b11120d6aaa5f2c3edf0e8
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2026-06-24 15:13:25 +0300

    Add Dagger qualifiers for application-scoped coroutine dispatchers

---
+++ app/src/main/java/com/androidinternals/musicplayer/core/coroutine/ApplicationScopeDefaultThread.kt
@@ -0,0 +1,7 @@
+package com.androidinternals.musicplayer.core.coroutine
+
+import jakarta.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScopeDefaultThread()
---
+++ app/src/main/java/com/androidinternals/musicplayer/core/coroutine/ApplicationScopeMainThread.kt
@@ -0,0 +1,7 @@
+package com.androidinternals.musicplayer.core.coroutine
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScopeMainThread()
